c1f1eefb2e565d8ea5e2659c7f4cf14361f562ed
snir2g1 <snir2g1@localhost>
2018-01-18 17:37:19 +0100

    cout"bonjour de moi" de rzt

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main(int argc, char** argv) {
     cout << "Proprement et chaudement" << endl;
     cout << "2 ème ajout de ligne tranquillement" << endl;
+    cout<<"Bonjour de moi"<<endl;
     return 0;
 }
 
