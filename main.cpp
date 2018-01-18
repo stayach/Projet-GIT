b75775a0d1dfbad337e3a92665454769566a52ef
samyt <stayach@btslivh.eu>
2018-01-18 17:36:38 +0100

    Ajout d'une deuxième ligne en commentaire

--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 int main(int argc, char** argv) {
-    cout << "Proprement et chaudement."<< endl;
-        
+    cout << "Proprement et chaudement." << endl;
+    cout << "2ème ajout de ligne tranquillement" << endl;
     return 0;
 }
 
