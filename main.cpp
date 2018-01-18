34b57b4ef1e9fa1581e31832aa5b8b6414182bd5
samyt <stayach@btslivh.eu>
2018-01-18 18:39:22 +0100

    Dernière compilation ajout dernière ligne

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main(int argc, char** argv) {
     cout << "Proprement et chaudement" << endl;
     cout << "2 ème ajout de ligne tranquillement" << endl;
+    cout << "Le travail est nécéssaire au développement "<< endl;
     return 0;
 }
 
